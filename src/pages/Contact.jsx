610bf40d1c7e424d5f28432aeba9b0d7b8a8d679
remus00 <135813551+remus00@users.noreply.github.com>
2023-11-12 18:59:53 +0100

    Finishing contact section and adding fox 3d model

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,18 @@
-import { useRef, useState } from 'react';
+import { Suspense, useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
+import { Canvas } from '@react-three/fiber';
+import Fox from '../models/Fox';
+import Loader from '../components/Loader';
+import useAlert from '../hooks/useAlert';
+import Alert from '../components/Alert';
 
 const Contact = () => {
     const formRef = useRef(null);
     const [form, setForm] = useState({ name: '', email: '', message: '' });
     const [isLoading, setIsLoading] = useState(false);
+    const [currentAnimation, setCurrentAnimation] = useState('idle');
+
+    const { alert, showAlert, hideAlert } = useAlert();
 
     const handleChange = (event) => {
         setForm({ ...form, [event.target.name]: event.target.value });
@@ -13,6 +21,7 @@ const Contact = () => {
     const handleSubmit = (event) => {
         event.preventDefault();
         setIsLoading(true);
+        setCurrentAnimation('hit');
 
         emailjs
             .send(
@@ -29,23 +38,35 @@ const Contact = () => {
             )
             .then(() => {
                 setIsLoading(false);
-                // TODO : SHOW SUCCESS MESSAGE
-                // TODO : HIDE ALLERT
-                setForm({ name: '', email: '', message: '' });
+
+                showAlert({ show: true, text: 'Message sent successfully!', type: 'success' });
+
+                setTimeout(() => {
+                    hideAlert();
+                    setCurrentAnimation('idle');
+                    setForm({ name: '', email: '', message: '' });
+                }, [3000]);
             })
             .catch((error) => {
                 setIsLoading(false);
+                setCurrentAnimation('idle');
                 console.log(error);
-                // TODO : SHOW ERROR MESSAGE
+                showAlert({ show: true, text: 'I didnt receive your message', type: 'danger' });
             });
     };
 
-    const handleFocus = () => {};
+    const handleFocus = () => {
+        setCurrentAnimation('walk');
+    };
 
-    const handleBlur = () => {};
+    const handleBlur = () => {
+        setCurrentAnimation('idle');
+    };
 
     return (
         <section className="relative flex lg:flex-row flex-col max-container">
+            {alert.show && <Alert {...alert} />}
+
             <div className="flex-1 min-w-[50%] flex flex-col">
                 <h1 className="head-text">Get in Touch</h1>
                 <form
@@ -104,6 +125,21 @@ const Contact = () => {
                         {isLoading ? 'Sending ...' : 'Send Message'}
                     </button>
                 </form>
+            </div>
+
+            <div className="lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]">
+                <Canvas camera={{ position: [0, 0, 5], fov: 75, near: 0.1, far: 1000 }}>
+                    <directionalLight intensity={2.5} position={[0, 0, 1]} />
+                    <ambientLight intensity={0.5} />
+                    <Suspense fallback={<Loader />}>
+                        <Fox
+                            currentAnimation={currentAnimation}
+                            position={[0.5, 0.35, 0]}
+                            rotation={[12.6, -0.6, 0]}
+                            scale={[0.5, 0.5, 0.5]}
+                        />
+                    </Suspense>
+                </Canvas>
             </div>
         </section>
     );
