a44ba1ca4cea9e1f64bd2206ed61934866da2eec
remus00 <135813551+remus00@users.noreply.github.com>
2023-11-12 17:50:05 +0100

    HomeInfo components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Island from '../models/Island';
 import Sky from '../models/Sky';
 import Bird from '../models/Bird';
 import Plane from '../models/Plane';
+import HomeInfo from '../components/HomeInfo';
 
 const Home = () => {
     const [isRotating, setIsRotating] = useState();
@@ -43,6 +44,10 @@ const Home = () => {
 
     return (
         <section className="w-full h-screen relative">
+            <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+                {currentStage && <HomeInfo currentStage={currentStage} />}
+            </div>
+
             <Canvas
                 className={`w-full h-screen bg-transparent ${
                     isRotating ? 'cursor-grabbing' : 'cursor-grab'
@@ -77,8 +82,3 @@ const Home = () => {
 };
 
 export default Home;
-{
-    /*  <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
-                POPUP
-            </div> */
-}
