b925fe700d57c3a6239901c7fa17e09e6f253b49
remus00 <135813551+remus00@users.noreply.github.com>
2023-11-12 19:57:54 +0100

    some fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -70,8 +70,8 @@ const Home = () => {
                         setCurrentStage={setCurrentStage}
                     />
                     <Plane
-                        planeScale={planeScale}
-                        planePosition={planePosition}
+                        scale={planeScale}
+                        position={planePosition}
                         isRotating={isRotating}
                         rotation={[0, 20, 0]}
                     />
